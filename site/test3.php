7a1cb31191c413adaaace3f85cbfd6aa5c8c6ac5
Joseph Zhong <glub.bob@gmail.com>
2015-01-17 01:01:01 -0800

    Confirmed that web URL works

--- site/test3.php
+++ site/test3.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: image/png');
 
-$bg = imagecreatefrompng('blueSquare.png');
+$bg = imagecreatefrompng('https://scontent-b.xx.fbcdn.net/hphotos-xap1/t31.0-8/s720x720/1973506_10202070758169184_5485992952186395972_o.jpg');
 $img = imagecreatefrompng('redSquare.png');
 
 imagecopy($bg, $img, 50, 50, 0, 0, imagesx($bg), imagesy($bg));
